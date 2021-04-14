12b857e2f71aaacd7693bd43cf095248cfe26b95
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-04-14 14:00:29 +0530

    commit

--- Google/Easy/Question2/Question2.java
+++ Google/Easy/Question2/Question2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner myObj = new Scanner(System.in);  
+	Scanner myObj = new Scanner(System.in);  
         System.out.println("Enter number of numbers");
         String n1 = myObj.nextLine(); 
         int n = Integer.parseInt(n1);
