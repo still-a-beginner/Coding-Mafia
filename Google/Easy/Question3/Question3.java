1d79f5f0996000fbd4d7b1ace19a8922b8e03739
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-04-19 23:44:58 +0530

    commit

--- Google/Easy/Question3/Question3.java
+++ Google/Easy/Question3/Question3.java
@@ -9,13 +9,13 @@ public class Main
         System.out.println("Enter m");
         String m1 = myObj.nextLine(); 
         int m = Integer.parseInt(m1);
-		int ans = 1;
+	int ans = 1;
         for(int i = m+n;i>n;i--)
         ans = ans * i;
         int ans1 = 1;
         for(int j = m;j>=1;j--)
         ans1 = ans1 * j;
         ans = ans / ans1;
-		System.out.println(ans);
+	System.out.println(ans);
 	}
 }
